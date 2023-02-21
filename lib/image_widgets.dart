d0863e84d06450eac5943dd48a6c5e731396c054
Deha Efecan Süer <dehasuer97@gmail.com>
2023-02-21 16:41:11 +0300

    files added

--- lib/image_widgets.dart
+++ lib/image_widgets.dart
@@ -11,37 +11,65 @@ class ImageOrnekleri extends StatelessWidget {
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEwJPZ4itSePdjATL65qFlgKFb-mr4vUO__g&usqp=CAU";
     return Center(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: Colors.red.shade300,
-            width: 150,
-            height: 150,
-            child: Image.asset(
-              "/Users/dehasuer/Desktop/Flutter_Apps/flutter_temel_widgets/assets/images/car.webp",
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            color: Colors.red.shade300,
-            width: 150,
-            height: 150,
-            child: Image.network(
-              _imgURL,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Container(
-              color: Colors.red.shade300,
-              child: CircleAvatar(
-                child: Text(
-                  "D",
-                  style: Theme.of(context).textTheme.headline1,
+          IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.red.shade300,
+                    child: Image.asset(
+                      "/Users/dehasuer/Desktop/Flutter_Apps/flutter_temel_widgets/assets/images/car.webp",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-                backgroundImage: NetworkImage(logo),
-                backgroundColor: Colors.amber,
-                foregroundColor: Colors.red,
-                radius: 90,
-              )),
+                Expanded(
+                  child: Container(
+                    color: Colors.red.shade300,
+                    width: 150,
+                    height: 150,
+                    child: Image.network(
+                      _imgURL,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                      color: Colors.red.shade300,
+                      child: CircleAvatar(
+                        child: Text(
+                          "D",
+                          style: Theme.of(context).textTheme.headline1,
+                        ),
+                        backgroundImage: NetworkImage(logo),
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.red,
+                      )),
+                ),
+              ],
+            ),
+          ),
+          IntrinsicHeight(
+            child: Container(
+              height: 200,
+              child: FadeInImage.assetNetwork(
+                  fit: BoxFit.cover,
+                  placeholder: "assets/images/loading.gif",
+                  image: _imgURL),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Placeholder(
+                color: Colors.blue,
+              ),
+            ),
+          )
         ],
       ),
     );
